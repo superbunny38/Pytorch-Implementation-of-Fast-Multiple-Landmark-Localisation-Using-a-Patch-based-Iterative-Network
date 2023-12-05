e6c19fe826053bc81f768caa69cc2d90ad1bb2c7
superbunny38 <superbunny38@gmail.com>
2023-12-05 16:18:42 +0900

    update rule done!

--- utils/update.py
+++ utils/update.py
@@ -17,4 +17,19 @@ def update_landmarks(landmarks, action_prob, yr_val, config):#update rule A,B,C
         row_ind = np.arange(landmarks.shape[0])
         landmarks[row_ind,ind] = landmarks[row_ind,ind] - yr_val[row_ind,ind]
 
+    elif config.predict_mode == 1:
+        landmarks = landmarks - yr_val*np.amax(np.reshape(action_prob, (landmarks.shape[0], landmarks.shape[1])), axis =2)
+
+    elif config.predict_mode == 2:
+        landmarks = landmarks - yr_val
+
+    elif config.predict_mode == 3:
+        step = 1
+        action_prob_reshape = np.reshape(action_prob, (landmarks.shape[0], landmarks.shape[1], 2))
+        ind = np.argmax(np.amax(action_prob_reshape, axis = 2), axis = 1)#ind = [num_exmples]
+        row_ind = np.arange(landmarks.shape[0])
+        is_negative = np.argmax(action_prob_reshape[row_ind,ind],axis = 1)
+        landmarks[row_ind[is_negative],ind[is_negative]] = landmarks[row_ind[is_negative],ind[is_negative]] + step
+        landmarks[row_ind[np.logical_not(is_negative)],ind[np.logical_not(is_negative)]] = landmarks[row_ind[np.logical_not(is_negative)],ind[np.logical_not(is_negative)]] - step
+
     return landmarks
